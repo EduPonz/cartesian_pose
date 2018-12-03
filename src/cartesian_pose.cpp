344e2571d280c31183b68cd4c1a4c823c3b51510
Eduardo Ponz Segrelles <e.ponzs@gmail.com>
2018-12-03 16:19:33 +0100

    Update cartesian_pose.cpp

--- src/cartesian_pose.cpp
+++ src/cartesian_pose.cpp
@@ -168,6 +168,8 @@ cart_pose CartesianPose::cartesian_pose(gps_position gps, float bearing_mag)
     set_last_yaw_acc_(temp_yaw_acc);
 
     ROS_INFO("****************************** GPS ******************************");
+    ROS_INFO_STREAM("GPS Coordiantes --> (" << gps.latitude << "; " << gps.longitude << ")");
+    ROS_INFO_STREAM("Cartesian Posit --> (" << pose.position.x << "; " << pose.position.y << ")");
     ROS_INFO_STREAM("Position ---------> (" << last_cartesian_.position.x << "; " << last_cartesian_.position.y << ")");
     ROS_INFO_STREAM("Velocity ---------> (" << last_velocity_.x << "; " << last_velocity_.y << ")");
     ROS_INFO_STREAM("Acceleration -----> (" << last_acceleration_.x << "; " << last_acceleration_.y << ")");
