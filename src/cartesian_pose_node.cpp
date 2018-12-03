a4ac9ef6ec3333e96d91b502fc1ddcc63afc2c5b
Eduardo Ponz Segrelles <e.ponzs@gmail.com>
2018-12-03 11:30:24 +0100

    add gnss node

--- src/cartesian_pose_node.cpp
+++ src/cartesian_pose_node.cpp
@@ -77,6 +77,7 @@ int main(int argc, char **argv)
             cartesian_log.ready_to_log = fix_state;
             publisher.publish(cartesian_log);
             prev_fix_state = fix_state;
+            ROS_INFO("State Changed!");
         }
 
         ros::spinOnce();
