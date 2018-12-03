1ab99422c692d00450226783dcd5f523e905c41a
Eduardo Ponz Segrelles <e.ponzs@gmail.com>
2018-12-03 12:42:39 +0100

    imu topic

--- src/cartesian_pose_node.cpp
+++ src/cartesian_pose_node.cpp
@@ -7,7 +7,7 @@
 #include "gy_88_interface/gy_88_lib.h"
 #include "gy_88_interface/Gy88Data.h"
 
-position gnss_position;
+gps_position gps_data;
 imu_data imu_data;
 bool new_imu = false;
 int fix_state = 0;
@@ -15,13 +15,9 @@ int prev_fix_state = -1;
 
 void gnss_data_callback(const gnss_l86_interface::GnssData::ConstPtr& gnss_msg)
 {
-    gnss_position.latitude = gnss_msg->latitude;
-    gnss_position.longitude = gnss_msg->longitude;
-    gnss_position.fix = gnss_msg->fix;
-    gnss_position.number_of_satelites = gnss_msg->number_of_satelites;
-    gnss_position.horizontal_precision = gnss_msg->horizontal_precision;
-    gnss_position.altitude = gnss_msg->altitude;
-    gnss_position.timestamp = gnss_msg->timestamp;
+    gps_data.latitude = gnss_msg->latitude;
+    gps_data.longitude = gnss_msg->longitude;
+    gps_data.timestamp = gnss_msg->timestamp;
     fix_state = gnss_msg->fix;
 }
 
@@ -41,27 +37,19 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::Publisher publisher = n.advertise<cartesian_pose::CartesianLog>("cartesian_log", 1000);
     ros::Subscriber gnss_sub = n.subscribe("gnss_data", 1000, gnss_data_callback);
-    ros::Subscriber imu_sub = n.subscribe("imu_data", 1000, imu_data_callback);
+    ros::Subscriber imu_sub = n.subscribe("gy88_data", 1000, imu_data_callback);
     ros::Rate loop_rate(1000);
     cartesian_pose::CartesianLog cartesian_log;
 
-    // gps_position north;
-    // north.latitude = 84.7;
-    // north.longitude = 169.77;
-    // north.timestamp = 0;
+    bool is_first_gps = true;
 
-    // gps_position ref;
-    // ref.latitude = 57.052968;
-    // ref.longitude = 9.915675;
-    // ref.timestamp = 0;
+    coordinates_2d vel;
+    vel.x = 0;
+    vel.y = 0;
 
-    // coordinates_2d vel;
-    // vel.x = 0;
-    // vel.y = 0;
-
-    // coordinates_2d acc;
-    // acc.x = 0;
-    // acc.y = 0;
+    coordinates_2d acc;
+    acc.x = 0;
+    acc.y = 0;
 
     // float bearing = -2.96452;
     // CartesianPose pose(north, ref, vel, acc, bearing);
@@ -87,6 +75,10 @@ int main(int argc, char **argv)
     {
         if (fix_state != prev_fix_state)
         {
+            // if (is_first_gps)
+            // {
+
+            // }
             cartesian_log.ready_to_log = fix_state;
             publisher.publish(cartesian_log);
             prev_fix_state = fix_state;
